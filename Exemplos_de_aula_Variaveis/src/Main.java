af844acade74ee99c1f8acb2d0f51c12a61379e3
IanLucasss <ianlucas.pessoal@gmail.com>
2025-03-20 19:08:25 -0300

    Exercício 1 lista 2

---
+++ Exemplos_de_aula_Variaveis/src/Main.java
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Meu programa vai aqui!
+
+        /* Este é
+        um comentário
+        de várias linhas
+         */
+
+        //declaração de variável
+        int n1= 5;
+        int n2= 2;
+
+        //aspas duplas = string
+        //aspas simples = char
+        //sout+tab= digito para print rápido
+
+        //operadores aritméticos
+        System.out.println(n1+n2); //7
+        System.out.println(n1-n2); //3
+        System.out.println(n1*n2); //10
+        System.out.println(n1/n2); //2.5
+        System.out.println(n1%n2); //1
+
+        //operadores lógicos
+        boolean b1 = true;
+        boolean b2 = false;
+
+        //lógica "E" = &&
+        //lógica "OU" = ||
+        //lógica "NOT" = !
+        System.out.println(b1 && b2);
+        System.out.println(b1 || b2);
+        System.out.println(b1 && !b2);
+        }
+    }
---
+++ Listas de Exercícios/LP_Lista_02/ExercIcio_01/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+int number;
+
+scanner in = new Scanner(system.in); //configura o scanner
+        number = in.nextInt();
+
+        result = number *2;
+        System.out.println("O dobro de " + number + "é" + result);
+        }
+    }
